e51ac6199eeec93572c98777503ca68e2dcd6c88
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 17:48:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,14 +8,9 @@ def introduction (name)
 
 end
 
-def introduction_with_language(name ,"Ruby". language)
+def introduction_with_language(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-
-
-
-
 
 
 
